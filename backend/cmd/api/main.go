070013ef3feed23753135786b82550d23cb57858
k0neru <koneneru@gmail.com>
2025-06-24 16:04:33 +0400

    feat[api]: add MSSQL database usage

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,21 +1,34 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"os"
 	"sync"
+	"time"
 
+	"internalwebBackend/internal/data"
 	"internalwebBackend/internal/jsonlog"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
+	models data.Models
 	wg     sync.WaitGroup
 }
 
@@ -24,17 +37,51 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 9000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "MSSQL data source name")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "MSSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "MSSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "MSSQL max commection idle time")
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
-	app := application{
-		config: cfg,
-		logger: logger,
-	}
-
-	err := app.serve()
+	db, err := openDB(cfg)
 	if err != nil {
 		logger.PrintFatal(err, nil)
 	}
+	defer db.Close()
+	logger.PrintInfo("database connection pool established", nil)
+
+	app := application{
+		config: cfg,
+		logger: logger,
+		models: data.NewModels(db),
+	}
+
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("mssql", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,13 @@ module internalwebBackend
 
 go 1.24.4
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/denisenkom/go-mssqldb v0.12.3
+	github.com/julienschmidt/httprouter v1.3.0
+)
+
+require (
+	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+)
