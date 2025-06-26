95cfbdbd8d42971c069a4c3c85ab3f44d51b1715
k0neru <koneneru@gmail.com>
2025-06-26 08:06:05 +0400

    feat[api]: add versioning in routes

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -5,8 +5,8 @@ import "github.com/julienschmidt/httprouter"
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
-	router.HandlerFunc("GET", "/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc("GET", "/phonebook", app.listPhonebookHandler)
+	router.HandlerFunc("GET", "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc("GET", "/v1/phonebook", app.listPhonebookHandler)
 
 	return router
 }
